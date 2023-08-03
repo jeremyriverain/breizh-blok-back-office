3462ac82ffe836751d300c7dd583259e30c4a6b3
Jérémy Riverain <jeremy@geekco.fr>
2023-08-03 20:01:07 +0200

    📈 try sentry as crash reporting solution

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -17,4 +17,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
 ];
