a6d235968a7964e4cbccdd4bdb509f38c6304f56
Jérémy Riverain <jeremy@geekco.fr>
2024-03-05 08:10:18 +0100

    ⬆️ replace ohmyfetch by ofetch

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -1,38 +1,40 @@
 const { defineConfig } = require("cypress");
-const { $fetch } = require('ohmyfetch')
+const { ofetch } = require("ofetch");
 
-const runBaseUrl = 'http://app_test'
+const runBaseUrl = "http://app_test";
 
-function getMailCatcherUrl (baseUrl) {
-    return baseUrl === runBaseUrl ? 'http://mailcatcher:1080' : 'http://localhost:1080'
+function getMailCatcherUrl(baseUrl) {
+  return baseUrl === runBaseUrl
+    ? "http://mailcatcher:1080"
+    : "http://localhost:1080";
 }
 
 module.exports = defineConfig({
   e2e: {
-      baseUrl: runBaseUrl,
-      viewportHeight: 800,
-      viewportWidth: 1280,
-      experimentalInteractiveRunEvents: true,
-      experimentalRunAllSpecs: true,
-      specPattern: 'cypress/integration/**/*.cy.js',
-      setupNodeEvents(on, config) {
-          on('before:run', async () => {
-              await $fetch(config.baseUrl + '/test/init-db')
-              return await $fetch(config.baseUrl + '/test/dump-db')
-            })
-          on('task', {
-            async loadDb() {
-              return await $fetch(config.baseUrl + '/test/load-db')
-            },
-            async fetchEmails() {
-              const mailcatcherUrl = getMailCatcherUrl(config.baseUrl)
-              return await $fetch(`${mailcatcherUrl}/messages`)
-            },
-            async fetchEmail(id) {
-              const mailcatcherUrl = getMailCatcherUrl(config.baseUrl)
-              return await $fetch(`${mailcatcherUrl}/messages/${id}.plain`)
-            },
-          })
+    baseUrl: runBaseUrl,
+    viewportHeight: 800,
+    viewportWidth: 1280,
+    experimentalInteractiveRunEvents: true,
+    experimentalRunAllSpecs: true,
+    specPattern: "cypress/integration/**/*.cy.js",
+    setupNodeEvents(on, config) {
+      on("before:run", async () => {
+        await ofetch(config.baseUrl + "/test/init-db");
+        return await ofetch(config.baseUrl + "/test/dump-db");
+      });
+      on("task", {
+        async loadDb() {
+          return await ofetch(config.baseUrl + "/test/load-db");
         },
+        async fetchEmails() {
+          const mailcatcherUrl = getMailCatcherUrl(config.baseUrl);
+          return await ofetch(`${mailcatcherUrl}/messages`);
+        },
+        async fetchEmail(id) {
+          const mailcatcherUrl = getMailCatcherUrl(config.baseUrl);
+          return await ofetch(`${mailcatcherUrl}/messages/${id}.plain`);
+        },
+      });
+    },
   },
 });
